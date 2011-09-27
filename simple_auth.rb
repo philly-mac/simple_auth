f82881c66bd4afc043057bedb22c916f1d8441c0
Philip MacIver <philip.maciver@xing.com>
2011-09-27 07:15:44 +0200

    added file for all the requires

---
+++ simple_auth.rb
@@ -0,0 +1,11 @@
+require 'lib/config'
+require 'lib/helpers'
+require 'lib/user_methods'
+
+require 'lib/methods/session_methods'
+require 'lib/methods/user_methods'
+
+require 'lib/http_verbs/session_methods'
+require 'lib/http_verbs/user_methods'
+
+require 'lib/properties/data_mapper'
