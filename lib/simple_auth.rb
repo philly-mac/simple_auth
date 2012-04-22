9c3547d39f5940478d4949693f31851fb891ecf4
Philip MacIver <philip.maciver@xing.com>
2012-04-22 07:53:28 +0200

    changing structure

---
+++ lib/simple_auth.rb
@@ -0,0 +1,26 @@
+base = File.dirname(File.expand_path(__FILE__))
+
+# Application root
+APP_ROOT = "#{File.dirname(File.expand_path(__FILE__))}/.."
+
+$: << "#{APP_ROOT}/lib"
+
+require "simple_auth/util"
+require "simple_auth/config"
+require "simple_auth/helpers"
+require "simple_auth/model_methods"
+
+require "simple_auth/setup"
+
+require "simple_auth/common/model_methods"
+require "simple_auth/common/session_methods"
+
+require "simple_auth/methods/session_methods"
+require "simple_auth/methods/model_methods"
+
+require "simple_auth/http_verbs/session_methods"
+require "simple_auth/http_verbs/model_methods"
+
+require "simple_auth/properties/data_mapper"
+require "simple_auth/properties/sequel"
+require "simple_auth/properties/mongoid"
