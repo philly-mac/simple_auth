0b5f8ec991b1f642f983a5ad70b97a891556be06
Philip MacIver <philip@ivercore.com>
2012-08-31 15:27:08 +0200

    Added scoping, so that you can have more than one type of user logged in

--- lib/simple_auth/helpers.rb
+++ lib/simple_auth/helpers.rb
@@ -1,30 +1,32 @@
 module SimpleAuth
   module Helpers
 
-    def authenticate!
-      unless current_user
+    def authenticate!(scope = :default)
+      unless current_user(scope)
         yield if block_given?
       end
     end
 
-    def current_user
-      unserialize_current_user
+    def current_user(scope = :default)
+      unserialize_current_user(scope)
     end
 
-    def current_user=(user)
-      serialize_current_user(user)
+    def current_user=(user, scope = :default)
+      serialize_current_user(user, scope)
     end
 
-    def log_out!
-      session[:logged_in_user] = nil
+    def log_out!(scope = :default)
+      if session[:logged_in_user]
+        session[:logged_in_user][scope] = nil
+      end
     end
 
-    def logged_in?
-      !current_user.nil?
+    def logged_in?(scope = :default)
+      !current_user(scope).nil?
     end
 
-    def logged_out?
-      !logged_in?
+    def logged_out?(scope = :default)
+      !logged_in?(scope)
     end
 
     def user_model
@@ -35,16 +37,17 @@ module SimpleAuth
 
   private
 
-    def serialize_current_user(user)
-      session[:logged_in_user] = user.id.to_s
+    def serialize_current_user(user, scope = :default)
+      session[:logged_in_user] = {} unless session[:logged_in_user]
+      session[:logged_in_user][scope] = user.id.to_s
     end
 
-    def unserialize_current_user
+    def unserialize_current_user(scope = :default)
       return @current_user if @current_user
-      return nil           if !session[:logged_in_user]
+      return nil           if !session[:logged_in_user] || !session[:logged_in_user][scope]
 
       method_call = defined?(::DataMapper) ? 'get' : 'find'
-      @current_user = user_model.send(method_call, session[:logged_in_user])
+      @current_user = user_model.send(method_call, session[:logged_in_user][scope])
       @current_user
     end
 
