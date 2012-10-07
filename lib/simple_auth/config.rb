a187c4e36331fef9ee34b95fb3b4e3ce770ac9d6
Philip MacIver <philip@ivercore.com>
2012-10-07 05:39:32 +0200

    Allow explict setting of underling library

---
+++ lib/simple_auth/config.rb
@@ -0,0 +1,24 @@
+module SimpleAuth
+  class Config
+
+    @@library = nil
+
+    def library=(library)
+      if [:datamapper, :sequel, :mongodb].include?(library)
+        @@library = library
+      else
+        @@library = nil
+        raise "#{library}: Not a valid SimpleAuth library"
+      end
+    end
+
+    def library
+      if @@library
+        @@library
+      else
+        raise "No SimpleAuth library set"
+      end
+    end
+
+  end
+end
